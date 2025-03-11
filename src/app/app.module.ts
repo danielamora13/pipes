58eeb7ef8f7e1f11d770e42663d4c420b6e098e6
danielamora13 <danielamora13@hotmail.com>
2025-03-11 09:54:46 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+import localeEsES from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEsES );
+registerLocaleData( localeFr );
 
 @NgModule({
   declarations: [
@@ -10,9 +22,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    SharedModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Lara
+      },
+      ripple: true,
+      inputVariant: 'filled'
+    }),
+    {
+      provide: LOCALE_ID, useValue: 'es'
+    }
+  ],
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
